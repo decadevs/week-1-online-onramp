5d8ea4a12766e1d6f8848082fbedb522947db029
Onyeike <100417124+Onyeike@users.noreply.github.com>
2022-02-26 10:34:10 +0100

    Challenge -1-
    function noofElements (x,y,z) {}

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -1,3 +1,17 @@
 function noOfElements(x, y, z) {}
 
-module.exports = noOfElements
+module.exports = noOfElements.
+let count =0;
+let length = x, length;
+
+For(let 1=0; 1<length;1+1) (
+  if (x[1]>=y ££x[1] <=z) {count++;
+                           }
+}
+return count;
+}
+console.log(noofElements([2,3,0,1,7],4,8));
+console.log(noofElements([3,3,3,3,5,18,6,20],15,30));
+console.log(noofElements([4,6,2,6,7,7,],0,7));
+module.exports = noofElements
+
