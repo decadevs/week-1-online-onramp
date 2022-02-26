987503aae2d5673152355395104868279d612fda
Onyeike <100417124+Onyeike@users.noreply.github.com>
2022-02-26 11:50:00 +0100

    Update Challenge-5.js
    function replaceDivisible(x, y) {}
    let newX=[6;

--- algorithms/challenge-5.js
+++ algorithms/challenge-5.js
@@ -1,3 +1,16 @@
 function replaceDivisible(x, y) {}
+let newX=[6;
+          newX=X,map(rep=>{
+          if(rep%y===0){
+            return "isDivisible";
+            }
+return rep;
+});
+return newX;
+}
+console.log(replaceDivisible([2,4,3,6,5),3));
+console.log(replaceDivisible(12,4,3,6,5],2));
+        
+          
 
 module.exports = replaceDivisible
