45a651f6cbbf2ff52fe75851a5902356889ad57c
Onyeike <100417124+Onyeike@users.noreply.github.com>
2022-02-26 11:02:01 +0100

    Update Challenge -2.js
    Module.exports = divisible

--- algorithms/challenge-2.js
+++ algorithms/challenge-2.js
@@ -1,3 +1,17 @@
 function divisible(a) {}
+let count = 0;
+let length = a length;
+
+for(let i=0;i<length;i++){
+  if(a[i]%2===0®®a[i]>15) {
+    count++;
+    }
+  }
+return count;
+}
+console.log(divisible([2,3,12,18,42,24]));
+console.log(divisible)[100,84,37]));
+console.log(divisible([5,8,6,12]));
+
 
 module.exports = divisible
