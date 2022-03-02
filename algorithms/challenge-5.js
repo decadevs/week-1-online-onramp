20174a9a7a236ce16cb5dcbb749eda5acc5a8607
Oluwaseun Akinruli <42046366+ruliseun@users.noreply.github.com>
2022-03-02 11:13:49 +0100

    Second commit

--- algorithms/challenge-5.js
+++ algorithms/challenge-5.js
@@ -1,15 +1,12 @@
 function replaceDivisible(x, y) {
-  let index = 0;
-
-  for (let i = 0; i < x.length; i++) {
-    if (x[i] % y === 0) {
-      index = x.indexOf(x[i]);
-      x[index] = "isDivisible";
-    } else {
-      x[index] = x[i];
+  let newArray = [];
+  newArray = x.map((value) => {
+    if (value % y === 0) {
+      value = "isDivisible";
     }
-    console.log(x);
-  }
+    return value;
+  });
+  return newArray;
 }
 replaceDivisible([2, 4, 3, 6, 5], 3);
 
